4244e5b0adfe881851db74c8586376751f2b6954
ChristinaMifsud <102205880+christina-mifsud@users.noreply.github.com>
2022-08-10 20:23:08 +0100

    testing carousel

--- personal-home-page-template/dist/script.js
+++ personal-home-page-template/dist/script.js
@@ -1,68 +1,19 @@
-/* =================
-  TESTS, LOOK AT THESE
-  Reading tests will always help you discover your requirements.
-  You can make this window bigger. 
-   ===================
-*/
+var images;
 
-const {
-  core: { test, expect, run },
-  prettify
-} = window.jestLite;
 
-/* =================
-  FIND ELEMENTS
-  These are all the elements we will look for.
-   ===================
-*/
-const getHeader = document.querySelectorAll("header"),
-  getH1 = document.querySelectorAll("h1"),
-  getSiteHeader = document.querySelectorAll(".c-site-header"),
-  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
-  getMain = document.querySelectorAll("main"),
-  getFooter = document.querySelectorAll("footer"),
-  getSiteFooter = document.querySelectorAll(".c-site-footer"),
-  getIFrame = document.querySelectorAll("iframe"),
-  getImage = document.querySelectorAll("img"),
-  getWords = document.body.innerText;
+images = ['https://imgur.com/NMe9Iij', 'https://imgur.com/vCqZdL0', 'https://imgur.com/ETCdKD5'];
 
-/* =================
-   ASSERTIONS 
-   These are the things we check are true about your page.
-   Read and update your HTML to discover the requirements.
-   The tests will run every time you update your code.
-   ===================
-*/
-test("There is at least one header element", () => {
-  expect(getHeader.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one h1", () => {
-  expect(getH1.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one header element with the class c-site-header", () => {
-  expect(getSiteHeader.length).toBe(1);
-});
-test("There is a nav element with an aria-label of Main Site Links.", () => {
-  expect(getAria.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one main element", () => {
-  expect(getMain.length).toBe(1);
-});
-test("There is at least one footer element", () => {
-  expect(getFooter.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one footer element with the class c-site-footer", () => {
-  expect(getSiteFooter.length).toBe(1);
-});
-test("There is embedded video", () => {
-  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one image", () => {
-  expect(getImage.length).toBeGreaterThanOrEqual(1);
-});
-test("There are at least 500 words on the page", () => {
-  expect(getWords.length).toBeGreaterThanOrEqual(500);
+
+document.getElementById('next').addEventListener('click', (event) => {
+  let element_carousel = document.getElementById('carousel');
+  element_carousel.setAttribute("src", images.slice(-1)[0]);
+  images.push(images.shift());
+
 });
 
-const console = document.getElementById("tests");
-prettify.toHTML(run(), console);
+document.getElementById('previous').addEventListener('click', (event) => {
+  let element_carousel2 = document.getElementById('carousel');
+  element_carousel2.setAttribute("src", images[0]);
+  images.unshift(images.pop());
+
+});
